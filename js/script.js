c36ad8a975ccffc7adbcd77a610e5c50a4a01fd9
vasya <nagetsy1234@mail.ru>
2021-09-24 23:58:35 +0300

    smth cool

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 
 let text = document.querySelector('.text');
 let btn = document.querySelector('.btn');
+let button = document.querySelector('.button');
 
 let arr = ['pidor', 'debil', 'norm guy', 'naruto', 'chert']
 let random = Math.floor(Math.random() * 5)
 
 btn.onclick = function () {
     text.innerHTML = arr[random];
+}
+
+button.onclick = function () {
+    text.innerHTML = 'Who I am?';
 }
